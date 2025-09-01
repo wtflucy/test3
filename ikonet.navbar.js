a22c1a46578be8f7f19323547c60e68307a4eff8
Isabela <wasntbela@gmail.com>
2025-09-01 08:05:59 +0200

    Add files via upload

---
+++ ikonet.navbar.js
@@ -0,0 +1,32 @@
+console.log("JS carregado!");
+
+class IkonetNavbar {
+  constructor(ikonetSelector, navListSelector, navLinksSelector) {
+    this.ikonet = document.querySelector(ikonetSelector);
+    this.navList = document.querySelector(navListSelector);
+    this.navLinks = document.querySelectorAll(navLinksSelector);
+    this.activeClass = "active";
+  }
+
+  addClickEvent() {
+    this.ikonet.addEventListener("click", () => {
+      console.log("hii"); 
+      this.navList.classList.toggle(this.activeClass);
+    });
+  }
+
+  init() {
+    if (this.ikonet) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new IkonetNavbar(
+  ".ikonet",      // seletor do botão hamburguer
+  ".nav-list",    // seletor da lista de links
+  ".nav-list li"  // seletor dos links individuais
+);
+
+mobileNavbar.init();
